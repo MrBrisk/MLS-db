79ad5931eb86b917136807525a239869eaeadfab
Rowan Murphy <rwnmurphy@gmail.com>
2023-10-19 23:29:58 -0500

    data setup WIP

--- database.js
+++ database.js
@@ -2,3 +2,73 @@ import mysql from 'mysql2';
 
 import dotenv from 'dotenv';
 dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+}).promise();
+
+export async function addTeam(team_id, team_name, team_name_abbrev, year_founded, year_joined, city) {
+  return await pool.query(`
+  INSERT IGNORE INTO team (team_id, team_name, team_name_abbrev, year_founded, year_joined, city)
+    VALUES(?, ?, ?, ?, ?, ?)`,
+    [team_id, team_name, team_name_abbrev, year_founded, year_joined, city]
+  );
+}
+
+export async function addPlayer(player_id, first_name, last_name, date_of_birth, nationality, height_cm, weight_kg, jersey_number, position) {
+  return await pool.query(`
+  INSERT IGNORE INTO player (player_id, first_name, last_name, date_of_birth, nationality, height_cm, weight_kg, jersey_number, position)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [player_id, first_name, last_name, date_of_birth, nationality, height_cm, weight_kg, jersey_number, position]
+  );
+}
+
+export async function addGame(game_id, date_played, home_team_score, away_team_score, home_team_id, away_team_id, game_type, round) {
+  return await pool.query(`
+  INSERT IGNORE INTO game (game_id, date_played, home_team_score, away_team_score, home_team_id, away_team_id, game_type, round)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
+    [game_id, date_played, home_team_score, away_team_score, home_team_id, away_team_id, game_type, round]
+  );
+}
+
+export async function addHomeTeamGame(team_id, game_id, season) {
+  return await pool.query(`
+  INSERT IGNORE INTO home_team_game (team_id, game_id, season)
+    VALUES(?, ?, ?)`,
+    [team_id, game_id, season]
+  );
+}
+
+export async function addAwayTeamGame(team_id, game_id, season) {
+  return await pool.query(`
+  INSERT IGNORE INTO away_team_game (team_id, game_id, season)
+    VALUES(?, ?, ?)`,
+    [team_id, game_id, season]
+  );
+}
+
+export async function addPlayerToTeamSeason(player_id, team_id, season) {
+  const games_played = await pool.query(`
+  SELECT count(*)
+    FROM player_game_stats
+    WHERE season = ? AND player_id = ? AND minutes_played > 0`, 
+    [season, player_id]
+  );
+  return await pool.query(`
+  INSERT IGNORE INTO player_team_season (player_id, team_id, season, games_played)
+    VALUES(?, ?, ?)`,
+    [player_id, team_id, season, games_played]
+  );
+}
+
+export async function addPlayerGameStats(player_id, game_id, season, goals, assists, saves, shots_faced, goals_conceded, shots_on_goal, minutes_played, yellow_cards, red_cards) {
+  return await pool.query(`
+  INSERT IGNORE INTO player_team_season (player_id, game_id, season, goals, assists, saves, shots_faced, goals_conceded, shots_on_goal, minutes_played, yellow_cards, red_cards)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [player_id, game_id, season, goals, assists, saves, shots_faced, goals_conceded, shots_on_goal, minutes_played, yellow_cards, red_cards]
+  );
+}
---
+++ mls_data/insertData.js
@@ -0,0 +1,83 @@
+import {
+  addGame,
+  addPlayer,
+  addTeam,
+  addAwayTeamGame,
+  addHomeTeamGame,
+  addPlayerGameStats,
+  addPlayerToTeamSeason,
+} from '../database.js';
+import fs from 'fs';
+
+const playersByTeamBySeason = [
+  JSON.parse(fs.readFileSync('mls_data/2021playersbyteam.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2022playersbyteam.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2023playersbyteam.json', 'utf-8')),
+];
+const matchDataBySeason = [
+  JSON.parse(fs.readFileSync('mls_data/2021matchstats1.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2021matchstats2.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2021matchstats3.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2022matchstats1.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2022matchstats2.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2022matchstats3.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2023matchstats1.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2023matchstats2.json', 'utf-8')),
+  JSON.parse(fs.readFileSync('mls_data/2023matchstats3.json', 'utf-8')),
+];
+const teamsMap = JSON.parse(fs.readFileSync('mls_data/teams.json', 'utf-8'));
+const playerIdMap = JSON.parse(fs.readFileSync('mls_data/playerIds.json', 'utf-8'));
+const gameIdMap = JSON.parse(fs.readFileSync('mls_data/gameIds.json', 'utf-8'));
+
+// insert teams
+console.log('Inserting teams...');
+for (let key in teamsMap) {
+  const team = teamsMap[key];
+  addTeam(team.team_id, team.team_name, team.team_name_abbrev, team.year_founded, team.year_joined, team.city);
+}
+
+// insert players
+console.log('Inserting players...');
+playersByTeamBySeason.forEach(season => {
+  season.season_competitor_players.forEach(team => {
+    team.players.forEach(player => {
+      addPlayer(playerIdMap[player.id], player.first_name, player.last_name, player.date_of_birth, player.nationality, player.height, player.weight, player.jersey_number, player.type);
+    });
+  });
+});
+
+// insert games
+console.log('Inserting games...');
+matchDataBySeason.forEach(chunk => {
+  chunk.summaries.forEach(summary => {
+    let home_team_id = null;
+    let away_team_id = null;
+    if (summary.sport_event_status.status != 'postponed' && summary.sport_event_status.status != 'cancelled' && summary.sport_event_status.status != 'not_started') {
+      summary.statistics.totals.competitors.forEach(team => {
+        if (team.qualifier == 'home') {
+          home_team_id = teamsMap[team.id].team_id;
+        } else if (team.qualifier == 'away') {
+          away_team_id = teamsMap[team.id].team_id;
+        }
+      });
+      let date = summary.sport_event.start_time.split('+')[0].replace('T', ' ');
+      let game_type = summary.sport_event.sport_event_context.stage.phase;
+      let round = game_type == 'regular season' ? null : summary.sport_event.sport_event_context.round.name;
+      addGame(gameIdMap[summary.sport_event.id], date, summary.sport_event_status.home_score, summary.sport_event_status.away_score, home_team_id, away_team_id, game_type, round);
+    }
+  });
+});
+
+// insert player game stats
+// console.log('Inserting player match stats...');
+
+// insert players to teams
+// console.log('Inserting players to teams...');
+
+// insert team home games
+// console.log('Inserting team home games...');
+
+// insert team away games
+// console.log('Inserting team away games...');
+
+console.log('Done');
